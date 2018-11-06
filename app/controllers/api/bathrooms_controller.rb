582eb1a3da593fa8a19ae542db1cb2868d362f83
miknhoj <kimjohan86@gmail.com>
2018-11-06 16:39:29 -0500

    Bathroom controller - index, show and create methods

--- app/controllers/api/bathrooms_controller.rb
+++ app/controllers/api/bathrooms_controller.rb
@@ -9,5 +9,19 @@ class Api::BathroomsController < ApplicationController
     render json: @bathroom
   end
 
+  def create
+    @bathroom = Bathroom.create!(bathroom_params)
+    render json: @bathroom
+  end
+
+  def update
+    
+  end
+
+
+private
+  def bathroom_params
+    params.require(:bathroom).permit(:street, :city, :state, :zip, :baby_changing, :toilets, :sinks)
+  end
 
 end
